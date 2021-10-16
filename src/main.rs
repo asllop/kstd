c478acc605c64da9853c6663927ed2042abd7383
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-10-16 20:30:44 +0200

    Abstract unsafe part.

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,18 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-unsafe fn set_video_data(index: i32, data: u8) {
-    *((0xB8000 + index) as *mut u8) = data;
+fn set_video_data(index: i32, data: u8) {
+    unsafe {
+        *((0xB8000 + index) as *mut u8) = data;
+    }
 }
 
 fn print_message(msg: &[u8]) {
-    unsafe {
-        let mut index = 0;
-        for ch in msg {
-            set_video_data(index, *ch);
-            index += 1;
-            set_video_data(index, 12); // light red
-            index += 1;
-        }
+    let mut index = 0;
+    for ch in msg {
+        set_video_data(index, *ch);
+        index += 1;
+        set_video_data(index, 12); // light red
+        index += 1;
     }
 }
